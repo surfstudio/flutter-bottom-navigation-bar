d762c9ca2e2d8ef3bcec256129fa09a44389496b
zhukeev <djukeevar@gmail.com>
2021-10-18 14:56:19 +0600

    main kt file

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.example
 
-import android.os.Bundle;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-import androidx.annotation.NonNull;
-public class MainActivity extends FlutterActivity {
-   
- @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+
+import io.flutter.embedding.android.FlutterActivity; 
+
+class MainActivity : FlutterActivity() {
+
 }
